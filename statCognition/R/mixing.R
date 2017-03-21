89e2481a1c1c04a0b581e79d75c1e3b9936d9080
klin1234@gmail.com <klin1234@gmail.com>
2017-03-21 11:58:06 -0400

    Made a crude mixing data function

--- statCognition/R/mixing.R
+++ statCognition/R/mixing.R
@@ -1,3 +1,30 @@
-# mixing_data <- function(mat, stopping_param = 0.75){
-#
-# }
+mixing_data <- function(mat, mixing_list = mixing_list_default(),
+                        distortion_param = 0.4, stopping_param = 0.8, base.seed = 10){
+
+  set.seed(base.seed)
+  while(TRUE){
+    idx_row <- sample(1:nrow(mat), round(distortion_param * runif(1) * nrow(mat)))
+    mat[idx_row,] <- mixing_list[[sample(1:length(mixing_list), 1)]](mat[idx_row,])
+
+    if(runif(1) >= stopping_param) break()
+  }
+
+  mat
+}
+
+mixing_list_default <- function(package_name = "statCognition",
+                                 function_starter = "mixing"){
+  if(!isNamespaceLoaded(package_name)) stop(paste(package_name, "is not",
+                                                  "currently loaded"))
+
+  all.obj <- ls(paste0("package:", package_name))
+
+  fun <- grep(paste0("^", function_starter, "_*"), all.obj, value = T)
+
+  lis <- lapply(fun, function(x){
+    eval(parse(text = paste0(package_name, "::", x)))
+  })
+  names(lis) <- fun
+
+  structure(lis, class = "mixing_list")
+}
--- statCognition/R/mixing_helpers.R
+++ statCognition/R/mixing_helpers.R
@@ -1,5 +1,12 @@
-.mixing_shuffle_columns <- function(mat, percent_columns = 0.1){
-  stopifnot(percentage_columns <= 1)
+#' Mixing function: Shuffle columns
+#'
+#' @param mat data matrix
+#' @param percent_columns percentage of columns affected
+#'
+#' @return numeric matrix
+#' @export
+mixing_shuffle_columns <- function(mat, percent_columns = 0.1){
+  stopifnot(percentage_columns <= 1, is.numeric(mat), is.matrix(mat))
 
   idx_columns <- sample(1:ncol(mat), round(percentage_columns * ncol(mat)))
   mat[,idx_columns] <- mat[,sample(idx_columns)]
@@ -7,8 +14,15 @@
   mat
 }
 
-.mixing_shuffle_rows <- function(mat, percent_columns = 0.1){
-  stopifnot(percentage_columns <= 1)
+#' Mixing function: Shuffle rows
+#'
+#' @param mat data matrix
+#' @param percent_columns percentage of columns affected
+#'
+#' @return numeric matrix
+#' @export
+mixing_shuffle_rows <- function(mat, percent_columns = 0.1){
+  stopifnot(percentage_columns <= 1, is.numeric(mat), is.matrix(mat))
 
   idx_columns <- sample(1:ncol(mat), round(percentage_columns * ncol(mat)))
   mat[,idx_columns] <- mat[sample(1:nrow(mat)), idx_columns]
@@ -16,32 +30,73 @@
   mat
 }
 
-.mixing_refit_normality <- function(mat){
+#' Mixing function: Refit from Gaussian distribution
+#'
+#' @param mat data matrix
+#'
+#' @return numeric matrix
+#' @export
+mixing_refit_normality <- function(mat){
+  stopifnot(is.numeric(mat), is.matrix(mat))
+
   mu <- colMeans(mat)
   Sigma <- stats::cov(mat)
 
   MASS::mvrnorm(nrow(mat), mu, Sigma)
 }
 
-.mixing_resample <- function(mat){
+#' Mixing function: Resample rows
+#'
+#' @param mat data matrix
+#'
+#' @return numeric matrix
+#' @export
+mixing_resample <- function(mat){
+  stopifnot(is.numeric(mat), is.matrix(mat))
+
   idx <- sample(1:nrow(mat), nrow(mat), replace = T)
   mat[idx,]
 }
 
-.mixing_swap_rows <- function(mat, idx1, idx2){
-  stopifnot(length(idx1) == length(idx2), length(intersect(idx1, idx2)) == 0)
+#' Mixing function: Swap rows
+#'
+#' @param mat data matrix
+#' @param idx1 indicies of rows
+#' @param idx2 indicies of rows
+#'
+#' @return numeric matrix
+#' @export
+mixing_swap_rows <- function(mat, idx1, idx2){
+  stopifnot(length(idx1) == length(idx2), length(intersect(idx1, idx2)) == 0,
+            is.numeric(mat), is.matrix(mat))
+
   mat[sample(idx1),] <- tmp; mat[sample(idx2),] <- tmp2
   mat[idx1,] <- tmp2; mat[idx2,] <- tmp
 
   mat
 }
 
-.mixing_adjust_mean <- function(mat, noise_func = function(x){x + sd(x)/2*rnorm(length(x))}){
+#' Mixing function: Adjust mean
+#'
+#' @param mat data matrix
+#' @param noise_func function to apply to each column of mat
+#'
+#' @return numeric matrix
+#' @export
+mixing_adjust_mean <- function(mat, noise_func = function(x){x + sd(x)/2*rnorm(length(x))}){
+  stopifnot(is.numeric(mat), is.matrix(mat))
   apply(mat, 1, function(y){noise_func(y)})
 }
 
-.mixing_shrink_correlation <- function(mat, percentage_columns = 0.1){
-  stopifnot(percentage_columns <= 1, ncol(mat) >= 2)
+#' Mixing function: Inflate correlation
+#'
+#' @param mat data matrix
+#' @param percent_columns percentage of columns affected
+#'
+#' @return numeric matrix
+#' @export
+mixing_inflate_correlation <- function(mat, percentage_columns = 0.1){
+  stopifnot(percentage_columns <= 1, ncol(mat) >= 2, is.numeric(mat), is.matrix(mat))
 
   idx <- sample(1:ncol(mat), floor(percentage_columns*ncol(mat)/2)*2)
   if(length(idx) <= 2) idx <- sample(ncol(mat), 2)
