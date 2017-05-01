ca92a045c30a7129ef82133d53d209d1e4223343
klin1234@gmail.com <klin1234@gmail.com>
2017-05-01 15:57:09 -0400

    Added monotone piecewise joint

--- statCognition/R/monotone_piecewise.R
+++ statCognition/R/monotone_piecewise.R
@@ -9,3 +9,19 @@
   structure(list(breakpoints = breakpoints, values = values, data = data),
             class = "mpm")
 }
+
+.monotone_piecewise_joint <- function(breakpoints_list, values_list){
+  stopifnot(length(breakpoints_list) == length(values_list))
+
+  mpm_list <- lapply(1:length(breakpoints_list), function(x){
+    .monotone_piecewise_marginal(breakpoints_list[[x]], values_list[[x]])
+  })
+
+  .mpj_from_mpm(mpm_list)
+}
+
+.mpj_from_mpm <- function(mpm_list){
+  stopifnot(class(mpm_list) == "list")
+
+  structure(mpm_list, class = "mpj")
+}
--- statCognition/tests/testthat/test_marginal_piecewise.R
+++ statCognition/tests/testthat/test_marginal_piecewise.R
@@ -9,3 +9,20 @@ test_that(".monotone_piecewise_marginal works", {
   expect_true(all(res$breakpoints == c(1,3,5,7)))
   expect_true(all(res$values == 10:13))
 })
+
+###############################
+
+## .monotone_piecewise_joint is correct
+
+test_that(".monotone_piecewise_joint works", {
+  breakpoint_lis <- list(c(1,3,5,7), c(2,4,6,8))
+  value_lis <- list(c(10,11,12,13), c(10,11,12,13))
+
+  res <- .monotone_piecewise_joint(breakpoint_lis, value_lis)
+
+  expect_true(length(res) == 2)
+  expect_true(class(res) == "mpj")
+  expect_true(unique(sapply(res, class)) == "mpm")
+})
+
+
