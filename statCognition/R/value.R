eed9656eab10a03a1811ee2c19c42010ffaededc
klin1234@gmail.com <klin1234@gmail.com>
2017-05-09 12:39:32 -0400

    Added documentation for value function

--- statCognition/R/value.R
+++ statCognition/R/value.R
@@ -1,3 +1,18 @@
+#' Value function
+#'
+#' \code{surface} is a list that contains \code{hash} and \code{table}.
+#' \code{hash} is a hash table with keys for example "1-5" (if there are two
+#' state elements, meaning the currently evaluated region lies in the first block
+#' of state element 1 and the fifth block of state element 5) and has a value that
+#' is a data frame with \code{value} set to the value of that block and \code{action}
+#' of the action taken in that block. \code{table} is a list of vectors with length
+#' equal to the number of state elements (2 in this case). It tells which regions in the
+#' domain of the state element correspond to which blocks.
+#'
+#' @param surface list of \code{hash} and \code{table}
+#'
+#' @return value object
+#' @export
 value <- function(surface){
   res <- structure(list(surface = surface), class = "value")
 
