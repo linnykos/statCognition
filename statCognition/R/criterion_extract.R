61644ff831f6130297f515e01a225427754f1ace
klin1234@gmail.com <klin1234@gmail.com>
2017-05-09 11:30:03 -0400

    Deleted mpm and mpj. Going to rename them properly as contribution and value

--- statCognition/R/criterion_extract.R
+++
@@ -1,7 +0,0 @@
-cognition_state <- function(){
-  .grab_package_contents("state_data", "state_list")
-}
-
-.apply_cognition_state <- function(state_list, mat, pheno, ...){
-  sapply(1:length(state_list), function(x){state_list[[x]](mat, pheno = pheno, ...)})
-}
