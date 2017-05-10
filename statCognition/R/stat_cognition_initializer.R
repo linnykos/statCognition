ed1d3410a1616294d1a06d7863004ab91c67a565
klin1234@gmail.com <klin1234@gmail.com>
2017-05-10 16:53:19 -0400

    Added stat cognition initializer test

--- statCognition/R/stat_cognition_initializer.R
+++ statCognition/R/stat_cognition_initializer.R
@@ -1,5 +1,8 @@
 stat_cognition_initializer <- function(action_ll, state_ll, difference_list = NA,
                                        generator_init = generate_synthetic_initializer()){
+  stopifnot(length(action_ll) == length(state_ll))
+  stopifnot(all(is.na(difference_list)) | length(difference_list) == length(state_ll))
+
   if(any(is.na(difference_list))){
     len <- length(action_ll)
     difference_list <- lapply(1:len, function(x){difference_states_indicator})
