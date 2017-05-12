d541cbad8539a1cae915c8c05155c33a55c3e9ba
klin1234@gmail.com <klin1234@gmail.com>
2017-05-12 01:55:09 -0400

    Starting to write the example and added the example data to package

---
+++ statCognition/R/data.R
@@ -0,0 +1,13 @@
+#' Example dataset
+#'
+#' @name dat
+#' @docType data
+#' @format A list with a matrix \code{mat} and data frame \code{pheno}. There
+#' are 107 samples.
+#' \describe{
+#'   \item{mat}{matrix with 2 columns}
+#'   \item{pheno}{data frame with 1 column, \code{Days}}
+#' }
+#' @author Kevin Lin \email{kevinl1@andrew.cmu.edu}
+#' @keywords data
+NULL
